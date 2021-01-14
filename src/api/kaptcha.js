f6ef3210dcad31de5e022711b13fb9a5028f745d
puck1 <yaodidu@gmail.com>
2021-01-14 11:08:55 +0800

    fix: lint and merge problems

--- src/api/kaptcha.js
+++ src/api/kaptcha.js
@@ -2,19 +2,19 @@ import request from '@/utils/request'
 
 export function defaultKaptcha() {
   return request({
-    url:'/kaptcha/defaultKaptcha',
-    method:'post',
-    responseType:'blob'
+    url: '/kaptcha/defaultKaptcha',
+    method: 'post',
+    responseType: 'blob'
   })
 }
 
-export function checkVerifyCode(code,verify_token) {
+export function checkVerifyCode(code, verify_token) {
   return request({
-    url:'/kaptcha/checkVerifyCode',
-    method:'post',
-    data:{
-      verifyCode :code,
-      verifyToken:verify_token
+    url: '/kaptcha/checkVerifyCode',
+    method: 'post',
+    data: {
+      verifyCode: code,
+      verifyToken: verify_token
     }
   })
 }
